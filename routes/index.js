3052337856ec52013281f25b701affd276235f69
robpolak <rob.polak@gmail.com>
2015-01-05 18:19:39 -0700

    weew

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,45 @@
 var express = require('express');
 var router = express.Router();
-
+var sm = require('sitemap');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'surfsterisp' });
 });
 
+router.get('/scrabble/word/finder/cheat/solver/', function(req, res) {
+  res.render('backlinks_sn', { title: ''});
+});
+
+
+
+var sitemap = sm.createSitemap ({
+  hostname: 'http://example.com',
+  cacheTime: 600000,        // 600 sec - cache purge period
+  urls: [
+    { url: '/',  changefreq: 'monthly', priority: 0.3 },
+    { url: '/scrabble/word/finder/cheat/solver/',  changefreq: 'monthly',  priority: 0.7 },
+  ]
+});
+
+router.get('/sitemap.xml', function(req, res) {
+  sitemap.toXML( function (xml) {
+    res.header('Content-Type', 'application/xml');
+    res.send( xml );
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
 router.get('*', function(req, res) {
   res.render('index', { title: 'surfsterisp'});
 });
-
 module.exports = router;
